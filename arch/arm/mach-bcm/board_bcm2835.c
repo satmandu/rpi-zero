97e104d1b945dfc4cbef6a45950f75e51f269c59
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2019-07-23 18:19:33 +0200

    ARM: bcm2835: register dmabounce on devices hooked to main interconnect
    NOTE: This patch builds upon Stefan's series providing basic support for
    RPi4[1]. I'm mostly interested in verifying if this is the correct approach
    to the issue stated below. If so I assume this will be added to Stefan's
    v2 series.
    
    The new Raspberry Pi 4 happens to have weird DMA constraints. Even
    though it might contain up to 4 GB of ram, most devices can only access
    the first lower GB of memory.
    
    This breaks the overall assumption DMA API makes whereas 32-bit DMA
    masks are always supported[2], and potentially breaks DMA addressing for
    all streaming DMA users. This has already been observed with
    'sdhci-iproc' but might as well happen elsewhere. Note that contiguous
    allocations are safe as 'dma_zone_size' is set accordingly.
    
    To get around that limitation we register arm's dmabounce dma-ops on all
    devices hooked to the SoC's main interconnect.
    
    [1] https://www.spinics.net/lists/arm-kernel/msg742120.html
    [2] https://www.spinics.net/lists/arm-kernel/msg742736.html
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- arch/arm/mach-bcm/board_bcm2835.c
+++ arch/arm/mach-bcm/board_bcm2835.c
@@ -3,6 +3,8 @@
  * Copyright (C) 2010 Broadcom
  */
 
+#include <linux/device.h>
+#include <linux/dma-mapping.h>
 #include <linux/init.h>
 #include <linux/irqchip.h>
 #include <linux/of_address.h>
@@ -24,8 +26,35 @@ static const char * const bcm2835_compat[] = {
 	NULL
 };
 
+static int bcm2835_needs_bounce(struct device *dev, dma_addr_t dma_addr, size_t size)
+{
+	/*
+	 * The accepted dma addresses are [0xc0000000, 0xffffffff] which map to
+	 * ram's [0x00000000, 0x3fffffff].
+	 */
+	return dma_addr < 3ULL * SZ_1G;
+}
+
+static int bcm2835_platform_notify(struct device *dev)
+{
+	if (dev->parent && !strcmp("soc", dev_name(dev->parent))) {
+		dev->dma_mask = &dev->coherent_dma_mask;
+		dev->coherent_dma_mask = DMA_BIT_MASK(30);
+		dmabounce_register_dev(dev, 2048, 4096, bcm2835_needs_bounce);
+	}
+
+	return 0;
+}
+
+void __init bcm2835_init_early(void)
+{
+	if(of_machine_is_compatible("brcm,bcm2711"))
+		platform_notify = bcm2835_platform_notify;
+}
+
 DT_MACHINE_START(BCM2835, "BCM2835")
 	.dma_zone_size	= SZ_1G,
 	.dt_compat = bcm2835_compat,
 	.smp = smp_ops(bcm2836_smp_ops),
+	.init_early = bcm2835_init_early,
 MACHINE_END
