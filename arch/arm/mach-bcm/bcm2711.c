3b653d345e583ea6edd54adaca8a5a3ad2769b34
Stefan Wahren <wahrenst@gmx.net>
2019-09-10 19:51:36 +0200

    ARM: mach-bcm: Fix depencency of bcm2711
    This fixes build issues in case ZONE_DMA isn't enabled.
    
    Reported-by: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Stefan Wahren <wahrenst@gmx.net>

--- arch/arm/mach-bcm/bcm2711.c
+++ arch/arm/mach-bcm/bcm2711.c
@@ -16,7 +16,9 @@ static const char * const bcm2711_compat[] = {
 };
 
 DT_MACHINE_START(BCM2711, "BCM2711")
+#ifdef CONFIG_ZONE_DMA
 	.dma_zone_size	= SZ_1G,
+#endif
 	.dt_compat = bcm2711_compat,
 	.smp = smp_ops(bcm2836_smp_ops),
 MACHINE_END
